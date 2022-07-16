ed6eaf9781798fe9dff54902427fe27c1b709edf
Wagner Cardoso <wagnerdev01@gmail.com>
2022-07-16 11:31:39 -0300

    feat(views/home) Add base styles

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,11 @@
 <template lang="pug">
-p Home
+v-container
+  v-row.text-center
+    v-col(cols="12")
+      v-icon(color="primary" x-large) task_alt
+    v-col.mb-4
+      h1.display-2.font-weight-bold.mb-3 Organize suas tarefas
+      p.subheading.font-weight-regular Faça o que você precisa
 </template>
 
 <script>
